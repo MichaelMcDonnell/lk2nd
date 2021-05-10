4c075d811251d7ee9538a067e3f6e63edc7d083a
Julian Braha <julianbraha@gmail.com>
2021-05-10 18:58:08 -0400

    motorola-ali: add to make rules

--- dts/rules.mk
+++ dts/rules.mk
@@ -2,12 +2,17 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 ifeq ($(PROJECT), msm8916-secondary)
 DTBS += \
+	$(LOCAL_DIR)/apq8016-samsung-r07.dtb \
 	$(LOCAL_DIR)/msm8916-longcheer-l8150.dtb \
-	$(LOCAL_DIR)/msm8916-samsung.dtb
+	$(LOCAL_DIR)/msm8916-motorola-harpia.dtb \
+	$(LOCAL_DIR)/msm8916-samsung-r01.dtb \
+	$(LOCAL_DIR)/msm8916-samsung-r03.dtb \
+	$(LOCAL_DIR)/msm8916-samsung-r04.dtb \
+	$(LOCAL_DIR)/msm8916-samsung-r06.dtb
 endif
 ifeq ($(PROJECT), msm8953-secondary)
 DTBS += \
-	$(LOCAL_DIR)/sdm450-samsung-r04.dtb \
+	$(LOCAL_DIR)/sdm450-motorola-ali.dtb \
 	$(LOCAL_DIR)/msm8953-xiaomi-mido.dtb \
 	$(LOCAL_DIR)/sdm632-motorola-ocean.dtb
 endif
